d42a59cef654ee81d7c6974b8daae3daafdcc4c3
Extravi <98912029+Extravi@users.noreply.github.com>
2023-01-06 18:08:58 -0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -4,7 +4,7 @@
  * @licstart  The following is the entire license notice for the
  *  JavaScript code in this page.
  * 
- * Copyright (C) 2023  Extravi, TEMtheLEM
+ * Copyright (C) 2023  Extravi
  * 
  * The JavaScript code in this page is free software: you can
  * redistribute it and/or modify it under the terms of the GNU Affero
