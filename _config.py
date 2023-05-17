c05aa4d7320bf8dae6e66042f086c03a295adabc
ttj <conninch@pm.me>
2023-05-17 17:46:06 -0400

    Fix UA & IP queries not being interpreted properly when additional characters are typo'd in & move valid IP & UA queries to `_config.py`

--- _config.py
+++ _config.py
@@ -32,3 +32,19 @@ user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15",
 ]
+
+# prompts for user agent & ip queries
+VALID_IP_PROMPTS = [
+    "what is my ip",
+    "what is my ip address",
+    "what's my ip",
+    "whats my ip"
+]
+VALID_UA_PROMPTS = [
+    "what is my user agent",
+    "what is my useragent",
+    "whats my useragent",
+    "whats my user agent",
+    "what's my useragent",
+    "what's my user agent",
+]
