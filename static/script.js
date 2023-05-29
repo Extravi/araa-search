aa846096fa6683b8fabcb219da74d93b211be03a
ttj <conninch@pm.me>
2023-05-29 19:16:02 -0400

    Hide material icons if the material icons file cannot be loaded.

--- static/script.js
+++ static/script.js
@@ -121,3 +121,14 @@ document.addEventListener('click', (event) => {
   }
 });
 
+// Load material icons. If the file cannot be loaded,
+// skip them and put a warning in the console.
+const font = new FontFace('Material Icons Round', 'url("/fonts/material-icons-round-v108-latin-regular.woff2") format("woff2")');
+font.load().then(() => {
+  document.querySelectorAll('#search-wrapper-ico, #sub-search-wrapper-ico').forEach((el) => {
+    // Show the icon if the font is successfully loaded
+    el.style.visibility = 'visible';
+  });
+}).catch(() => {
+  console.warn('Failed to load Material Icons Round. Hiding any icons using said pack.');
+});
