d31bf7d847e62efff1b1964e8c2edea249143df4
amogusussy <amogussussy@protonmail.com>
2023-08-13 00:13:52 +0100

    Remove old syntax file reading

--- __init__.py
+++ __init__.py
@@ -9,10 +9,6 @@ from src import textResults, torrents, helpers, images, video
 with open("./bangs.json", "r") as bfp:
     bjson = json.loads(bfp.read())
 
-bfp = open("./bangs.json", "r")
-bjson = json.load(bfp)
-bfp.close()
-
 app = Flask(__name__, static_folder="static", static_url_path="")
 app.jinja_env.filters['highlight_query_words'] = helpers.highlight_query_words
 app.jinja_env.globals.update(int=int)
