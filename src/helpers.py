6ef3bc16514819e5bc0f5d1589960d220717da56
Extravi <98912029+Extravi@users.noreply.github.com>
2023-09-20 13:36:38 -0400

    Update helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -28,12 +28,14 @@ def makeHTMLRequest(url: str):
 def highlight_query_words(string, query):
     query_words = [re.escape(word) for word in query.lower().split()]
     words = string.split()
+    highlighted = []
+    query_regex = re.compile('|'.join(query_words))
     highlighted_words = []
     for word in words:
-        cleaned_word = word.strip()
-        if any(re.search(query_word, cleaned_word, re.IGNORECASE) for query_word in query_words):
-            highlighted_word = re.sub('|'.join(query_words), lambda m: f'<span class="highlight">{escape(m.group())}</span>', cleaned_word, flags=re.IGNORECASE)
-            highlighted_words.append(highlighted_word)
+        cleaned_word = word.strip().lower()
+        if query_regex.search(cleaned_word) and cleaned_word not in highlighted:
+            highlighted_words.append(Markup(f'<span class="highlight">{escape(word)}</span>'))
+            highlighted.append(cleaned_word)
         else:
             highlighted_words.append(escape(word))
     return Markup(' '.join(highlighted_words))
