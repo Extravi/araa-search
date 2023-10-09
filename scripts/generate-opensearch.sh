c4c2f8493dca3f176c838efb43a25fec1244f7df
Extravi <98912029+Extravi@users.noreply.github.com>
2023-10-09 06:22:54 -0400

    Update generate-opensearch.sh

--- scripts/generate-opensearch.sh
+++ scripts/generate-opensearch.sh
@@ -5,7 +5,7 @@
 if [ $DOMAIN ]; then
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <OpenSearchDescription xmlns=\"http://a9.com/-/spec/opensearch/1.1/\">
-  <ShortName>TailsX</ShortName>
+  <ShortName>Araa</ShortName>
   <Url type=\"text/html\" template=\"https://$DOMAIN/search?q={searchTerms}\"/>
   <Url rel=\"suggestions\" type=\"application/x-suggestions+json\" method=\"get\" template=\"https://$DOMAIN/suggestions?q={searchTerms}\" />
   <OutputEncoding>UTF-8</OutputEncoding>
