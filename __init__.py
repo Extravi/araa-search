e75ce09ed053f9acb0861312d3db388e744fcb00
TEMtheLEM <conninch@pm.me>
2023-11-21 13:34:53 -0500

    Fix a possible redirect error when saving changes to settings.

--- __init__.py
+++ __init__.py
@@ -37,6 +37,10 @@ def settings():
     with open(json_path, 'r') as file:
         lang_data = json.load(file)
 
+    # Upgrade the request URL to https as to prevent a redirection error with /save-settings.
+    if not request.is_secure:
+        request.url = f"https://{request.host}/settings{f'?{request.query_string.decode()}' if request.query_string.decode() else ''}"
+
     return render_template('settings.html',
                            theme=theme,
                            lang=lang,
