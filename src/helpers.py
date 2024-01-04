69c54e0cf62229d929460e54cbbb410e123d908b
Extravi <98912029+Extravi@users.noreply.github.com>
2024-01-03 21:44:04 -0500

    Update helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -42,7 +42,7 @@ def makeHTMLRequest(url: str, is_google=False):
         cookies = {}
 
     headers = {
-        "User-Agent": random.choice(user_agents),  # Choose a user-agent at random
+        "User-Agent": random.choice(user_agents), # Choose a user-agent at random
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Encoding": "gzip, deflate",
         "Accept-Language": "en-US,en;q=0.5",
