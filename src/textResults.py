db83183075382c9b69092af19f63b6c6af784f0f
Extravi <98912029+Extravi@users.noreply.github.com>
2024-04-18 18:46:31 -0400

    Update textResults.py

--- src/textResults.py
+++ src/textResults.py
@@ -35,9 +35,9 @@ def textResults(query) -> Response:
         if search_type == "reddit":
             site_restriction = "site:reddit.com"
             query_for_request = f"{query} {site_restriction}"
-            soup = helpers.makeHTMLRequest(f"https://www.google.com{settings.domain}&q={quote(query_for_request)}&start={p}&lr={settings.lang}", is_google=True)
+            soup = helpers.makeHTMLRequest(f"https://www.google.com{settings.domain}&q={quote(query_for_request)}&start={p}&lr={settings.lang}&num=20", is_google=True)
         elif search_type == "text":
-            soup = helpers.makeHTMLRequest(f"https://www.google.com{settings.domain}&q={quote(query)}&start={p}&lr={settings.lang}&safe={settings.safe}", is_google=True)
+            soup = helpers.makeHTMLRequest(f"https://www.google.com{settings.domain}&q={quote(query)}&start={p}&lr={settings.lang}&safe={settings.safe}&num=20", is_google=True)
         else:
             return "Invalid search type"
     except Exception as e:
